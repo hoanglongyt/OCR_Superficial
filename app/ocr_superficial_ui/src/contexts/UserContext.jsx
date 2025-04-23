649e06f58f68bf555ecbc197cd44511bc7b6cb4b
Hùng Cường <cuongdayne17@gmail.com>
2025-04-23 09:39:45 +0700

    [update] use mkcert as the major certificate issuer

--- app/ocr_superficial_ui/src/contexts/UserContext.jsx
+++ app/ocr_superficial_ui/src/contexts/UserContext.jsx
@@ -3,7 +3,10 @@ import React, { useState, createContext } from "react";
 export const UserContext = createContext(null)
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(() => {
+        const savedUserToken = localStorage.getItem("token");
+        return savedUserToken ? savedUserToken : null
+    })
 
     return (
         <UserContext.Provider value={{user, setUser}}>
